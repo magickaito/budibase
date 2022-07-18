c71b5f0dc39f18098110ceb6dec3ff298ad93dfc
Terrence Tee <terrence@tplusinteractive.com>
2022-07-18 11:30:57 +0800

    remove bottom right support link, update title and favicon

--- packages/builder/src/App.svelte
+++ packages/builder/src/App.svelte
@@ -3,8 +3,6 @@
   import { routes } from "../.routify/routes"
   import { NotificationDisplay, BannerDisplay } from "@budibase/bbui"
   import { parse, stringify } from "qs"
-  import HelpIcon from "components/common/HelpIcon.svelte"
-
   const queryHandler = { parse, stringify }
 </script>
 
@@ -14,7 +12,6 @@
 <NotificationDisplay />
 <Router {routes} config={{ queryHandler }} />
 <div class="modal-container" />
-<HelpIcon />
 
 <style>
   .modal-container {
